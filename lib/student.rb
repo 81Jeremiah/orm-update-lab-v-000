8d1033f809d7c306e8365312dee3a184f13b4f08
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-05-28 17:04:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,4 +33,15 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    if self.id
+      self.update
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?,?);
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_row() FROM students")[0][0]
+    
 end
