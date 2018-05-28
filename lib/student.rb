e00345a6156aaf37df395e8dcc13ae884c908301
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-05-28 16:54:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,12 @@ class Student
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE IF EXISTS students;
+    SQL
 
+    DB[:conn].execute(sql)
+  end
 
 end
