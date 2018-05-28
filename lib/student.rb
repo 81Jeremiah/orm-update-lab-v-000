cd498c33224fac6c24f1c0ae8b5f21926eb32548
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-05-28 17:30:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,17 @@ class Student
     new_student.grade = row[2]
     new_student
   end
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    SQL
+
+  DB[:conn].execute(sql,name).collect do |row|
+    self.new_from_db(row)
+  end.first
+end
 
   def update
     sql = <<-SQL
