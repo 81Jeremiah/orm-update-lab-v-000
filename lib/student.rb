9dd86b9197c1f55c26acefddb1be1270309396aa
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-05-28 17:11:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
       SQL
 
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_row() FROM students")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
 
